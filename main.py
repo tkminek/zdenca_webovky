73f087a4294b3ce4b400376bb91f4a8713150533
tkminek <kminek.tomas@seznam.cz>
2021-07-17 20:09:00 +0200

    product page

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ app = Flask(__name__)
 def main_page():
     return "<h1>sanity test</h1><p> checked </p>"
 
+@app.route('/products', methods=['GET'])
+def products_page():
+    return "<h1>ZDENCINA NABIDKA</h1><p> produkt 1 </p>"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
