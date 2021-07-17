ecf2ca1eda92722ac41d5431b8f5f818097647f9
tkminek <kminek.tomas@seznam.cz>
2021-07-17 21:19:39 +0200

    add products, contacts

--- main.py
+++ main.py
@@ -6,12 +6,15 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def main_page():
-    return "<h1>sanity test</h1><p> checked </p>"
+    return render_template('main_page.html')
 
 @app.route('/products', methods=['GET'])
 def products_page():
-    return "<h1>ZDENCINA NABIDKA</h1><p> produkt 1 </p>"
+    return render_template('products_page.html')
 
+@app.route('/contact', methods=['GET'])
+def contact_page():
+    return render_template('contact_page.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
