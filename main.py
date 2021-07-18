fb3d1e9037e82cef137d401e51e9964d6cd3392f
tkminek <kminek.tomas@seznam.cz>
2021-07-18 17:37:41 +0200

    add order page

--- main.py
+++ main.py
@@ -12,9 +12,18 @@ def main_page():
 def products_page():
     return render_template('products_page.html')
 
-@app.route('/contact', methods=['GET'])
-def contact_page():
-    return render_template('contact_page.html')
+@app.route('/order', methods=['GET','POST'])
+def order_page():
+    if request.method == 'GET':
+        return render_template('order_page.html')
+    elif request.method == 'POST':
+        number_of_product=request.form["product_number"]
+        quantity = request.form["quantity"]
+        email = request.form["email"]
+        with open ("orders.txt","a") as orders_file:
+            orders_file.write(f"{email} si objednava {quantity}x produkt číslo: {number_of_product}\n")
+
+        return render_template("order_finished_page.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
